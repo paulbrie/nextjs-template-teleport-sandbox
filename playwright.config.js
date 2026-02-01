b68f18b39f47b40340f1280c69c5663106ed3ca7
Paul <paul.brie@teleporthq.io>
2026-02-01 14:04:13 -0500

    first commit

---
+++ playwright.config.js
@@ -0,0 +1,24 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./tests",
+  timeout: 30000,
+  expect: {
+    timeout: 5000,
+  },
+  reporter: "line",
+  use: {
+    actionTimeout: 0,
+    trace: "on-first-retry",
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+    // {
+    //   name: "firefox",
+    //   use: { ...devices["Desktop Firefox"] },
+    // },
+  ],
+});
